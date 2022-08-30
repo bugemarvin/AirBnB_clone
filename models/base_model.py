f7e28a897c883c45f3ae25e3c3f673fdbd51d3be
Marvin Kurland <bugemarvin@outlook.com>
2022-08-31 00:16:41 +0300

    initial commits

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
 '''
 contians all defined attributes/methods for other classes.
 '''
 
 class BaseModel:
-    def __init__(self, id):
-        id = uuid.uuid4()
-        self.id = id
+    def __init__(self, id, date):
+        self.id = uuid.uui4()
+        self.date = datetime.now()
+    def created_at(self):
+        return self.date
+    def updated_at(self, updated_date):
+        self.date = updated_date
