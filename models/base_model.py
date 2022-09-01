1d914205b7d61d22a4c8fab877c02e1281bc1642
Marvin Kurland <bugemarvin@outlook.com>
2022-09-01 12:12:46 +0300

    adding bool for instances date time

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,11 @@ contians all defined attributes/methods for other classes.
 '''
 
 class BaseModel:
-    def __init__(self, id, created_at, updated_at):
-        self.id = uuid.uui4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(*arg, **kwargs):
+        self.id = str(uuid.uui4())
+	if isinstance(Basemode, self.id) == True:
+            self.created_at = datetime.now()
+            if created_at in BaseModel not None:
+                self.updated_at = datetime.now()
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
