de6e5cb5d3d787ae7543befd3a96555a388be391
Marvin Kurland <bugemarvin@outlook.com>
2022-09-02 00:51:18 +0300

    instaciating kwags with public instances

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
         you can use uuid.uuid4() to generate unique id but don’t forget to convert to a string
         the goal is to have unique id for each BaseModel
     created_at: datetime - assign with the current datetime when an instance is created
-    updated_at: datetime - assign with the current datetime when an instance is created 
+    updated_at: datetime - assign with the current datetime when an instance is created
                 and it will be updated every time you change your object
     '''
     id = None
@@ -19,12 +19,12 @@ class BaseModel:
     updated_at = None
     def __init__(self, *args, **kwargs):
         '''
-        you will use *args, **kwargs arguments for the constructor of a BaseModel. 
+        you will use *args, **kwargs arguments for the constructor of a BaseModel.
 	                                 (more information inside the AirBnB clone concept page)
         *args won’t be used
         if kwargs is not empty:
-                            each key of this dictionary is an attribute name 
-                                  (Note __class__ from kwargs is the only one that should not be 
+                            each key of this dictionary is an attribute name
+                                  (Note __class__ from kwargs is the only one that should not be
                                          added as an attribute. See the example output, below)
                             each value of this dictionary is the value of this attribute name
         Warning: created_at and updated_at are strings in this dictionary, but inside your BaseModel instance
@@ -50,7 +50,7 @@ class BaseModel:
     '''
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     '''
     For Debuging incase __str__() fails
     '''
@@ -59,14 +59,14 @@ class BaseModel:
         Returns dunder __str__() if it fails.
 	'''
         return self.__str__()
-    
+
     '''
     To update public instance update_at for created already created instance.
     with the current date of the updated obj in it's instance class.
     '''
     def save(self):
         pass
-    
+
     '''
     create format of output for the dunder __dict__ of an instance created.
     '''
