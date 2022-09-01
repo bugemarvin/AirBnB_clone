ca53130628d4873c044faed3a1d7a8735733bf0d
Marvin Kurland <bugemarvin@outlook.com>
2022-09-02 00:08:16 +0300

    instaciating kwags with public instances

---
+++ 1-ts_mld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,72 @@
 #!/usr/bin/python3
-from uuid import uuid4 UUID
+import uuid
 from datetime import datetime
 '''
-contians all defined attributes/methods for other classes.
+class BaseModel that defines all common attributes/methods for other classes.
 '''
 
 class BaseModel:
     '''
-    creating class variables for the base model.
+    id: string - assign with an uuid when an instance is created:
+        you can use uuid.uuid4() to generate unique id but don’t forget to convert to a string
+        the goal is to have unique id for each BaseModel
+    created_at: datetime - assign with the current datetime when an instance is created
+    updated_at: datetime - assign with the current datetime when an instance is created 
+                and it will be updated every time you change your object
     '''
-    base_date = datetime.now()
-    def __init__(*arg, **kwargs):
-	'''
-        Declaring public instance attribute for id.
+    id = None
+    created_at = None
+    updated_at = None
+    def __init__(self, *args, **kwargs):
         '''
-        self.id = str(uuid.uui4())
-        self.created_at = self.base_date
-        self.updated_at = self.base_date
+        you will use *args, **kwargs arguments for the constructor of a BaseModel. 
+	                                 (more information inside the AirBnB clone concept page)
+        *args won’t be used
+        if kwargs is not empty:
+                            each key of this dictionary is an attribute name 
+                                  (Note __class__ from kwargs is the only one that should not be 
+                                         added as an attribute. See the example output, below)
+                            each value of this dictionary is the value of this attribute name
+        Warning: created_at and updated_at are strings in this dictionary, but inside your BaseModel instance
+                 is working with datetime object. You have to convert these strings into datetime object.
+        '''
+        if kwargs:
+            for key,value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+        if not BaseModel.id:
+            self.id = str(uuid.uuid4())
+        if not BaseModel.created_at:
+            self.created_at = self.updated_at = datetime.now()
+        if not BaseModel.updated_at:
+            self.updated_at = datetime.now()
     '''
     Using Dunder method __str__() for out put.
     '''
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    '''For Debuging incase __str__() fails'''
+    
+    '''
+    For Debuging incase __str__() fails
+    '''
     def __repl__(self):
         '''
         Returns dunder __str__() if it fails.
 	'''
         return self.__str__()
+    
     '''
     To update public instance update_at for created already created instance.
     with the current date of the updated obj in it's instance class.
     '''
     def save(self):
         pass
+    
     '''
     create format of output for the dunder __dict__ of an instance created.
     '''
